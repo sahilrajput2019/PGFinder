5d9ca2a3521404bea8de06b0ffde5f27138a387f
Sahil Rajput <sahilrajput85579@gmail.com>
2021-03-24 20:47:57 +0530

    Added visibility of .env file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 const App = () => {
-  return <div>Hello from App</div>;
+  return <div>Landing Page</div>;
 };
 
 export default App;
